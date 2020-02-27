094adf2e53bc6f11c9b78c6d57dd83fc3e389fc8
zc0rp10 <zcorpio@gmail.com>
2020-02-27 10:38:45 +0100

    play, pause, scrub fw & bwd functionality

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require("path");
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1366,
+    height: 768,
     webPreferences: {
       preload: path.join(__dirname, "./src/preload.js")
     }
--- src/index.js
+++ src/index.js
@@ -5,24 +5,57 @@
 // selectively enable features needed in the rendering
 // process.
 
-var audio = new Audio("../audiobooks/audiobook.m4b");
-const playButton = document.getElementById("play-audio-btn");
-const pauseButton = document.getElementById("pause-audio-btn");
-const progressBar = document.getElementById("progress-bar-fill");
+$ = document.getElementById.bind(document);
+let audio = new Audio("../audiobooks/audiobook.m4b");
+const playBtn = $("play-audio-btn");
+const pauseBtn = $("pause-audio-btn");
+const scrubFwdBtn = $("skip-forward-btn");
+const scrubBwdBtn = $("skip-backward-btn");
+const progressBar = $("progress-bar-fill");
+const barCurrentTime = $("bar-current-time");
+const barTotalTime = $("bar-total-time");
 
-playButton.addEventListener("click", () => {
+function secondsToHms(d) {
+  d = Number(d);
+  let h = Math.floor(d / 3600);
+  let m = Math.floor((d % 3600) / 60);
+  let s = Math.floor((d % 3600) % 60);
+
+  let hDisplay = h > 0 ? (h < 10 ? "0" + h : h) : "00";
+  let mDisplay = m > 0 ? (m < 10 ? "0" + m : m) : "00";
+  let sDisplay = s > 0 ? (s < 10 ? "0" + s : s) : "00";
+  return `${hDisplay}:${mDisplay}:${sDisplay}`;
+}
+
+// Player Btn Events
+playBtn.addEventListener("click", () => {
   audio.play();
-  console.log("Play Button Pressed");
+  playBtn.style.display = "none";
+  pauseBtn.style.display = "inline-block";
+  console.log("Play Btn Pressed");
 });
 
-pauseButton.addEventListener("click", () => {
+pauseBtn.addEventListener("click", () => {
   audio.pause();
+  pauseBtn.style.display = "none";
+  playBtn.style.display = "inline-block";
   console.log("Pause Button Pressed");
 });
 
+scrubFwdBtn.addEventListener("click", () => {
+  audio.currentTime = audio.currentTime + 30;
+});
+
+scrubBwdBtn.addEventListener("click", () => {
+  audio.currentTime = audio.currentTime - 30;
+});
+
+// Progressbar & Timestamp Updates
 const handleProgress = () => {
   const percent = (audio.currentTime / audio.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
+  barCurrentTime.innerHTML = secondsToHms(audio.currentTime);
+  barTotalTime.innerHTML = secondsToHms(audio.duration);
 };
 
 audio.addEventListener("timeupdate", handleProgress);
